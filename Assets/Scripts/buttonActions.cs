695586302e6543f6fe55e58add44adb6a11eb4ab
kolotw <77420523+kolotw@users.noreply.github.com>
2025-05-18 15:23:14 +0800

    0518_Recording

---
+++ Assets/Scripts/buttonActions.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonActions : MonoBehaviour
+{
+    //«ö¶s°Ê§@
+    
+    public void ­º­¶«ö¶s()
+    {
+        SceneManager.LoadScene("00_Homepage");
+    }
+
+}
